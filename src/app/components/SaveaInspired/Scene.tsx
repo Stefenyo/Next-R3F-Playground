bd421dc72e19625161e92df26b2f64776b419932
Stefenyo <stephenioabbott@gmail.com>
2024-07-07 02:05:30 -0400

    updated bg color

--- src/app/components/SaveaInspired/Scene.tsx
+++ src/app/components/SaveaInspired/Scene.tsx
@@ -7,7 +7,7 @@ import { AnimatedCircles } from "./AnimatedCircles";
 // Inspired by https://www.savea.com/
 function Scene() {
   return (
-    <div style={{ width: "100vw", height: "100vh", background: "#0C0000" }}>
+    <div style={{ width: "100vw", height: "100vh", background: "#000000" }}>
       <Canvas camera={{ position: [0, 0, 5], fov: 75 }}>
         <Suspense fallback={null}>
           <AnimatedCircles />
