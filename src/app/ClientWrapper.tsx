07aa7b766f35fe0652488faac0844f5bb2971119
Stephenio Abbot <stephenioabbott@gmail.com>
2025-03-02 01:40:13 -0500

    added indicators scene

--- src/app/ClientWrapper.tsx
+++ src/app/ClientWrapper.tsx
@@ -3,10 +3,17 @@
 import dynamic from "next/dynamic";
 
 // Use dynamic import with no SSR for the Three.js component
-const DiamondScene = dynamic(() => import("@/components/scenes/DiamondScene"), {
-  ssr: false,
-});
+// const CirclesScene = dynamic(() => import("@/components/scenes/CirclesScene"), {
+//   ssr: false,
+// });
+
+const IndicatorsScene = dynamic(
+  () => import("@/components/scenes/IndicatorsScene"),
+  {
+    ssr: false,
+  }
+);
 
 export default function ClientWrapper() {
-  return <DiamondScene />;
+  return <IndicatorsScene />;
 }
